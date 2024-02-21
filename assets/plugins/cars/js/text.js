80873fd0d79021b31758395fbdb9d423176b784f
bejsuman5 <bejsuman5@gmail.com>
2024-02-21 20:15:26 +0530

    create new project celebration

---
+++ assets/plugins/cars/js/text.js
@@ -0,0 +1,64 @@
+const text = "অভিমানের মেঘ উড়িয়ে দাও অনেক দূরে,\nমন খারাপের দিনটা তোমার না আসুক ফিরে,\nদুঃখ গুলো দাউ উড়িয়ে ওই আকাশের নীচে,\nঅসীম সুখ বয়ে আসুক তোমার জীবন ভোরে,\nশুভ জন্মদিন প্রিয় ।।";
+const giftBoxHeading ="Open The Surprise Box !";
+const neonText1 ="H";
+const neonText2 ="A";
+const neonText3 ="P";
+const neonText4 ="P";
+const neonText5 ="Y";
+const neonText6 ="B";
+const neonText7 ="I";
+const neonText8 ="R";
+const neonText9 ="T";
+const neonText10 ="H";
+const neonText11 ="D";
+const neonText12 ="A";
+const neonText13 ="Y";
+
+
+const neonText101 ="M";
+const neonText102 ="Y";
+const neonText103 ="D";
+const neonText104 ="E";
+const neonText105 ="A";
+const neonText106 ="R";
+const neonText107 ="F";
+const neonText108 ="R";
+const neonText109 ="I";
+const neonText110 ="E";
+const neonText111 ="N";
+const neonText112 ="D";
+const neonText113 ="!";
+
+
+
+document.getElementById("giftBoxHeading").innerHTML = giftBoxHeading;
+
+
+document.getElementById("neonText1").innerHTML = neonText1;
+document.getElementById("neonText2").innerHTML = neonText2;
+document.getElementById("neonText3").innerHTML = neonText3;
+document.getElementById("neonText4").innerHTML = neonText4;
+document.getElementById("neonText5").innerHTML = neonText5;
+document.getElementById("neonText6").innerHTML = neonText6;
+document.getElementById("neonText7").innerHTML = neonText7;
+document.getElementById("neonText8").innerHTML = neonText8;
+document.getElementById("neonText9").innerHTML = neonText9;
+document.getElementById("neonText10").innerHTML = neonText10;
+document.getElementById("neonText11").innerHTML = neonText11;
+document.getElementById("neonText12").innerHTML = neonText12;
+document.getElementById("neonText13").innerHTML = neonText13;
+
+
+document.getElementById("neonText101").innerHTML = neonText101;
+document.getElementById("neonText102").innerHTML = neonText102;
+document.getElementById("neonText103").innerHTML = neonText103;
+document.getElementById("neonText104").innerHTML = neonText104;
+document.getElementById("neonText105").innerHTML = neonText105;
+document.getElementById("neonText106").innerHTML = neonText106;
+document.getElementById("neonText107").innerHTML = neonText107;
+document.getElementById("neonText108").innerHTML = neonText108;
+document.getElementById("neonText109").innerHTML = neonText109;
+document.getElementById("neonText110").innerHTML = neonText110;
+document.getElementById("neonText111").innerHTML = neonText111;
+document.getElementById("neonText112").innerHTML = neonText112;
+document.getElementById("neonText113").innerHTML = neonText113;
