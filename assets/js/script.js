8ca36f39f4e8d2a36bf702d8d1094ba8b776bc64
bejsuman5 <bejsuman5@gmail.com>
2024-02-23 15:24:03 +0530

    modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,9 +207,43 @@ for (let i = 0; i < NUM_STARS; i++) {
 
 
 
-// gift-box 
+// change the image depended by the user name
 // *************************************************************************************
 // *************************************************************************************
+
+// Get the folder name from the URL query parameter, with a default value of 'default-folder'
+const urlParams = new URLSearchParams(window.location.search);
+const birthday_person = urlParams.get('birthday_person') || 'flowers';
+
+// Set the image source based on the folder name
+const imgElement1 = document.getElementById('image-1');
+imgElement1.src = `assets/plugins/${birthday_person}/images/1.jpg`;
+
+const imgElement2 = document.getElementById('image-2');
+imgElement2.src = `assets/plugins/${birthday_person}/images/2.jpg`;
+
+const imgElement3 = document.getElementById('image-3');
+imgElement3.src = `assets/plugins/${birthday_person}/images/3.jpg`;
+
+const imgElement4 = document.getElementById('image-4');
+imgElement4.src = `assets/plugins/${birthday_person}/images/4.jpg`;
+
+const imgElement5 = document.getElementById('image-5');
+imgElement5.src = `assets/plugins/${birthday_person}/images/5.jpg`;
+
+const imgElement6 = document.getElementById('image-6');
+imgElement6.src = `assets/plugins/${birthday_person}/images/6.jpg`;
+
+const imgElement7 = document.getElementById('image-7');
+imgElement7.src = `assets/plugins/${birthday_person}/images/7.jpg`;
+
+const imgElement8 = document.getElementById('image-8');
+imgElement8.src = `assets/plugins/${birthday_person}/images/8.jpg`;
+
+
+
+
+
 // gift-box 
 // *************************************************************************************
 // *************************************************************************************
