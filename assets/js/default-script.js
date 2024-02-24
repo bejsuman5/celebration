fa7d65423ab1c2fa44a100ebf4c70061a4b0c8e4
bejsuman5 <bejsuman5@gmail.com>
2024-02-24 15:35:24 +0530

    modify

--- assets/js/default-script.js
+++ assets/js/default-script.js
@@ -1,5 +1,5 @@
 const urlLocation = new URLSearchParams(window.location.search);
-const birthdayPersonNameForScript = urlLocation.get('birthday_person') || 'flowers';
+const birthdayPersonNameForScript = urlLocation.get('birthday_person') || 'default';
 const scriptElement = document.createElement('script');
 
 scriptElement.src = `assets/plugins/${birthdayPersonNameForScript}/js/text.js`;
--- assets/js/script.js
+++ assets/js/script.js
@@ -216,32 +216,32 @@ for (let i = 0; i < NUM_STARS; i++) {
 
 // Get the folder name from the URL query parameter, with a default value of 'default-folder'
 const urlParams = new URLSearchParams(window.location.search);
-const birthday_person = urlParams.get('birthday_person') || 'flowers';
+const birthday_person = urlParams.get('birthday_person') || 'default';
 
 // Set the image source based on the folder name
 const imgElement1 = document.getElementById('image-1');
-imgElement1.src = `assets/plugins/${birthday_person}/images/1.jpg`;
+imgElement1.src = `assets/plugins/${birthday_person}/images/image-1.jpg`;
 
 const imgElement2 = document.getElementById('image-2');
-imgElement2.src = `assets/plugins/${birthday_person}/images/2.jpg`;
+imgElement2.src = `assets/plugins/${birthday_person}/images/image-2.jpg`;
 
 const imgElement3 = document.getElementById('image-3');
-imgElement3.src = `assets/plugins/${birthday_person}/images/3.jpg`;
+imgElement3.src = `assets/plugins/${birthday_person}/images/image-3.jpg`;
 
 const imgElement4 = document.getElementById('image-4');
-imgElement4.src = `assets/plugins/${birthday_person}/images/4.jpg`;
+imgElement4.src = `assets/plugins/${birthday_person}/images/image-4.jpg`;
 
 const imgElement5 = document.getElementById('image-5');
-imgElement5.src = `assets/plugins/${birthday_person}/images/5.jpg`;
+imgElement5.src = `assets/plugins/${birthday_person}/images/image-5.jpg`;
 
 const imgElement6 = document.getElementById('image-6');
-imgElement6.src = `assets/plugins/${birthday_person}/images/6.jpg`;
+imgElement6.src = `assets/plugins/${birthday_person}/images/image-6.jpg`;
 
 const imgElement7 = document.getElementById('image-7');
-imgElement7.src = `assets/plugins/${birthday_person}/images/7.jpg`;
+imgElement7.src = `assets/plugins/${birthday_person}/images/image-7.jpg`;
 
 const imgElement8 = document.getElementById('image-8');
-imgElement8.src = `assets/plugins/${birthday_person}/images/8.jpg`;
+imgElement8.src = `assets/plugins/${birthday_person}/images/image-8.jpg`;
 
 
 
