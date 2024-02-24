adccc63552457be774d59433e0fe558c9bcefcba
bejsuman5 <bejsuman5@gmail.com>
2024-02-24 16:04:59 +0530

    modify

--- assets/plugins/cars/js/text.js
+++
@@ -1,7 +0,0 @@
-const giftBoxText = "অভিমানের মেঘ উড়িয়ে দাও অনেক দূরে,\nমন খারাপের দিনটা তোমার না আসুক ফিরে,\nদুঃখ গুলো দাউ উড়িয়ে ওই আকাশের নীচে,\nঅসীম সুখ বয়ে আসুক তোমার জীবন ভোরে,\nশুভ জন্মদিন প্রিয় ।।";
-const giftBoxHeading ="Open The Surprise Box ! (Cars)";
-
-
-
-
-document.getElementById("giftBoxHeading").innerHTML = giftBoxHeading;
