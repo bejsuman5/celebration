357c814426417777afb75f4db9663bf430231c77
Your bejsuman5 <bejsuman5@gmail.com>
2024-04-16 22:22:44 +0530

    modify

--- assets/js/default-script.js
+++ assets/js/default-script.js
@@ -5,28 +5,7 @@ const scriptElement = document.createElement('script');
 scriptElement.src = `assets/plugins/${birthdayPersonNameForScript}/js/text.js`;
 
 scriptElement.onerror = function() {
-    // scriptElement.src = 'assets/js/default-text.js'; // Set another source location
     window.location.href = 'index.html';
 };
-// scriptElement.addEventListener('error', () => {
-//     scriptElement.src = 'fallback-location.js'; // Set another source location
-// });
-
-// const scriptLoadSuccess = () => {
-//     // Function to run if the script was loaded successfully
-//     console.log('Script loaded successfully!');
-// };
-
-// const scriptLoadError = () => {
-//     // Function to run if the script failed to load
-//     console.log('Script failed to load!');
-//     window.location.href = 'index.html';
-// };
-
-// // Add an event listener for the script's onload event
-// scriptElement.onload = scriptLoadSuccess;
-
-// // Add an event listener for the script's onerror event
-// scriptElement.onerror = scriptLoadError;
 
 document.head.appendChild(scriptElement);
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,8 @@ const giftBoxContainer = document.querySelector('.gift-box-container');
 const gifBoxBody = document.querySelector('.gift-box-body');
 const giftBoxShadow = document.querySelector('.gift-box-shadow');
 const giftBoxTextBody = document.querySelector('.gift-box-text-body');
-// const click2 = document.querySelector('.click2');
-// const typingAnimation = document.querySelector('.typing-animation');
-const giftBoxContent = document.getElementById('giftBoxContent');
 const giftBoxTextTypingAnimation = document.getElementById('giftBoxTextTypingAnimation');
 const giftBoxCapId = document.getElementById('giftBoxCap');
-// const giftBoxText = "অভিমানের মেঘ উড়িয়ে দাও অনেক দূরে,\nমন খারাপের দিনটা তোমার না আসুক ফিরে,\nদুঃখ গুলো দাউ উড়িয়ে ওই আকাশের নীচে,\nঅসীম সুখ বয়ে আসুক তোমার জীবন ভোরে,\nশুভ জন্মদিন প্রিয় ।।";
-const giftBoxCountdown = document.getElementById('giftBoxCountdown');
 const giftBtn = document.querySelector('.gift-btn');
 
 // gift-box click function
@@ -26,9 +21,7 @@ giftBoxCapClass.addEventListener('click', () => {
         giftBoxTextBody.classList.add('active');
         giftBoxTextBody.classList.remove('active2');
         giftBtn.classList.add('active');
-        // click2.classList.add('active');
-        // click.classList.add('hide');
-        // typingAnimation.classList.add('animate');
+
         typingTimeout = setTimeout(typeWriter, 2000);
     }
     else {
@@ -39,10 +32,7 @@ giftBoxCapClass.addEventListener('click', () => {
         giftBoxShadow.classList.remove('active');
         giftBoxTextBody.classList.remove('active');
         giftBoxTextBody.classList.add('active2');
-        // giftBtn.classList.remove('active');
-        // click2.classList.remove('active');
-        // click.classList.remove('hide');
-        // typingAnimation.classList.remove('animate');
+
         resetText();
     }
 });
@@ -68,36 +58,9 @@ function resetText() {
     index = 0;
 }
 
-
-// change the image depended by the user name
-// *************************************************************************************
-// *************************************************************************************
-
-// Get the folder name from the URL query parameter, with a default value of 'default-folder'
 const urlParams = new URLSearchParams(window.location.search);
 const birthday_person = urlParams.get('birthday_person') || 'default';
 
-// Set the image source based on the folder name
-const imgElement1 = document.getElementById('image-1');
-imgElement1.src = `assets/plugins/${birthday_person}/images/image-1.jpg`;
-
-const imgElement2 = document.getElementById('image-2');
-imgElement2.src = `assets/plugins/${birthday_person}/images/image-2.jpg`;
-
-const imgElement3 = document.getElementById('image-3');
-imgElement3.src = `assets/plugins/${birthday_person}/images/image-3.jpg`;
-
-const imgElement4 = document.getElementById('image-4');
-imgElement4.src = `assets/plugins/${birthday_person}/images/image-4.jpg`;
-
-const imgElement5 = document.getElementById('image-5');
-imgElement5.src = `assets/plugins/${birthday_person}/images/image-5.jpg`;
-
-const imgElement6 = document.getElementById('image-6');
-imgElement6.src = `assets/plugins/${birthday_person}/images/image-6.jpg`;
-
-const imgElement7 = document.getElementById('image-7');
-imgElement7.src = `assets/plugins/${birthday_person}/images/image-7.jpg`;
-
-const imgElement8 = document.getElementById('image-8');
-imgElement8.src = `assets/plugins/${birthday_person}/images/image-8.jpg`;
+const baseURL = 'index4.html';
+let responsiveURL = baseURL +'?birthday_person='+ birthday_person;
+document.getElementById('giftBtnId').setAttribute('href', responsiveURL);
