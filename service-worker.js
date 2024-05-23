cc5f7f9a16a896bf4434375ede9aa2fd1007293f
Your bejsuman5 <bejsuman5@gmail.com>
2024-05-23 16:39:41 +0530

    test

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('push', function(event) {
+    let data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        badge: 'assets/images/badge.png', // optional
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
