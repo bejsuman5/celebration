0a8458b2ed1cb85410e35478a37aae98f4c984b2
Your bejsuman5 <bejsuman5@gmail.com>
2024-05-23 16:19:03 +0530

    test

---
+++ assets/js/push-notification.js
@@ -0,0 +1,10 @@
+// const clickMe = document.getElementById("clickMe");
+
+// clickMe.addEventListener("click", () => {
+//     // alert('Hello World');
+//     Notification.requestPermission().then(perm => {
+//         if (perm === "granted") {
+//             new Notification("example notification")
+//         }
+//     })
+// })
