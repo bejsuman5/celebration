c74b4040cbb9aec122618f31b322d00d37e410d0
Your bejsuman5 <bejsuman5@gmail.com>
2024-05-24 21:40:42 +0530

    12

---
+++ old/test-project/12/service-worker.js
@@ -0,0 +1,50 @@
+// self.addEventListener('push', function(event) {
+//     let data = {};
+//     if (event.data) {
+//         data = event.data.json();
+//     }
+
+//     const options = {
+//         body: data.body,
+//         icon: data.icon,
+//         badge: 'assets/images/badge.png', // optional
+//     };
+
+//     event.waitUntil(
+//         self.registration.showNotification(data.title, options)
+//     );
+// });
+
+// self.addEventListener('notificationclick', function(event) {
+//     event.notification.close();
+//     event.waitUntil(
+//         clients.openWindow('/')
+//     );
+// });
+
+
+// --------------------------------------------------
+
+self.addEventListener('push', function(event) {
+    let data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        badge: data.badge
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(function(clientList) {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
