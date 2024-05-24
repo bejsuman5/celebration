f8006e8c1377171f4e3e9b0b620ec5002f96726e
Your bejsuman5 <bejsuman5@gmail.com>
2024-05-24 19:58:21 +0530

    add notification

---
+++ old/service-worker1.js
@@ -0,0 +1,45 @@
+// self.addEventListener('push', function(event) {
+//     let data = {};
+//     if (event.data) {
+//         data = event.data.json();
+//     }
+
+//     const options = {
+//         body: data.body,
+//         icon: data.icon,
+//         badge: 'assets/images/badge.png', // optional
+//     };
+
+//     event.waitUntil(
+//         self.registration.showNotification(data.title, options)
+//     );
+// });
+
+// self.addEventListener('notificationclick', function(event) {
+//     event.notification.close();
+//     event.waitUntil(
+//         clients.openWindow('/')
+//     );
+// });
+
+
+// --------------------------------------------------
+
+self.addEventListener('push', function(event) {
+    let data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        badge: data.badge
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
+});
