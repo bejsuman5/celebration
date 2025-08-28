d0092f1013911d9e4e1d33c3774c096dd837192f
bejsuman <bejsuman5@gmail.com>
2025-08-29 01:19:06 +0530

    happy birthday ajoy

---
+++ assets/plugins/ajay/js/text.js
@@ -0,0 +1,3 @@
+const giftBoxText = "অভিমানের মেঘ উড়িয়ে দাও অনেক দূরে,\nমন খারাপের দিনটা তোমার না আসুক ফিরে,\nদুঃখ গুলো দাউ উড়িয়ে ওই আকাশের নীচে,\nঅসীম সুখ বয়ে আসুক তোমার জীবন ভোরে,\nশুভ জন্মদিন ।।";
+const giftBoxHeading ="Open The Surprise Box !";
+document.getElementById("giftBoxHeading").innerHTML = giftBoxHeading;
